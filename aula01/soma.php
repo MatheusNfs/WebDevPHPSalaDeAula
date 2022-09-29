91bcb0a18cf6321dc88f26ff92e9bfe997b5ecb2
Matheus Nascimento Ferreira da Silva <matheus.dev2022@gmail.com>
2022-09-28 21:30:03 -0300

    Primeira Aula de PHP

---
+++ aula01/soma.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+        $num1="4002";
+        echo "</br><h3>primeiro número é $num1</h3>";
+        $num2=3999;
+        echo "</br><h3>segundo número é $num2</h3>";
+        if($num1+$num2>8000){
+            printf("<h2>A soma </h2><h1>é mais de 8000!</h1>");
+        }
+        printf("<h2>resultado da soma dos dois números é de ".$num1 + $num2."</h2>");
+
+    
+    ?>
+
+</body>
+</html>
