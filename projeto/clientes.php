04aa20c2301b02ea2f968835e95ad17a19312de7
Matheus Nascimento Ferreira da Silva <matheus.dev2022@gmail.com>
2022-11-24 20:51:41 -0300

    Confirmaçao js

--- projeto/clientes.php
+++ projeto/clientes.php
@@ -6,24 +6,26 @@
 	<!--  -->
 	<body>
 		<?php 
-		include "html/header.php";
 		require_once "src/conexao.php";
+		require_once "src/protect.php";
 		
-		$lista=[];
-		$sql_code = "SELECT * FROM cliente";
-		$sql_query = $conexao->query($sql_code);
-
-		if($sql_query->num_rows > 0){
-			$lista = $sql_query->fetch_all(MYSQLI_ASSOC);
-			// var_dump($lista);
-		}
+		
 		if(!isset($_SESSION)){
 			session_start();
 		}
 		$id = isset($_SESSION['id']) ? $_SESSION['id'] : 0;
 		$nome = isset($_SESSION['nome']) ? $_SESSION['nome'] : 0;
+		$tipo = isset($_SESSION['tipo']) ? $_SESSION['tipo'] : 0;
 		
-	
+		$lista=[];
+		$sql_code = "SELECT * FROM cliente";
+		$sql_query = $conexao->query($sql_code);
+		
+		if($sql_query->num_rows > 0){
+			$lista = $sql_query->fetch_all(MYSQLI_ASSOC);
+			// var_dump($lista);
+		}
+		
 		
 		
 		?>
@@ -61,38 +63,22 @@
 					<td><?= $cliente["email"]; ?></td>
 					<td><?= $cliente["ativo"]; ?></td>
 					<td>
-						<a href="edicaoCliente.php?id=<?=$cliente["idcliente"]; ?>">EDITAR</a>
-						<?php
-							if(isset($_SESSION['tipo']) && $_SESSION['tipo'] == "G"){
-								$idc = $cliente['idcliente'];
-								echo "<a href='#?id=$idc'; data-bs-toggle='modal' data-bs-target='#excluir_modal'>EXCLUIR</a>";
-							}
-						?>					
+						<a href="edicaoCliente.php?id=<?=$cliente['idcliente']; ?>">
+                        <!-- [EDITAR] -->
+                        <i class="bi bi-pencil-square" style="font-size: 1.15rem;"></i>
+                        </a>
+
+                       <?php if(isset($_SESSION['tipo']) && $_SESSION['tipo'] == "G"){?>
+						<a href="#" onclick="confirmarApagar(<?=$cliente['idcliente']; ?>)">
+
+                        <!-- src/controler/cliente_bd/excluirCliente.php?id= -->
+                        <!-- [EXCLUIR] -->
+                         <i class="bi bi-trash" style="font-size: 1.15rem; color: red;"></i>
+                        </a>
+                       <?php } ?>
 						
 					</td>
-					<td>
-					<div class="modal fade" id="excluir_modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-						<div class="modal-dialog modal-dialog-centered">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
-									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-								</div>
-								<form action="src/controller/cliente_bd/excluirCliente.php" method="post" >
-									<div class="modal-body">
-									<input type="text" value="<?=$cliente['idcliente'];?>">
-									<h2 style="text-align: center;">Deseja realmente excluir este cliente? <?= $cliente['nome']; ?></h2>	
-														
-									</div>
-									<div class="modal-footer">
-										<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-										<button type="submit" class="btn btn-danger">Confirmar</button>
-									</div>
-								</form>
-							</div>
-						</div>
-					</div>
-					</td>
+					
 				<?php endforeach ?>	
 				</tr>
 			</table>
@@ -101,7 +87,7 @@
 		</main>
 		<?php include "html/rodape.php" ?>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+		<script src="src/js/confirmacao.js"></script>
 </body>
 
 </html>
--- projeto/src/controller/cliente_bd/excluirCliente.php
+++ projeto/src/controller/cliente_bd/excluirCliente.php
@@ -3,15 +3,20 @@
 require_once "../../protect.php";
 require_once "../../conexao.php";
 
-$idCliente = isset($_POST['idcliente']) ? $_POST['idcliente'] : 0;
+if(isset($_SESSION['tipo']) && $_SESSION['tipo'] == "G"){
 
-if($idCliente > 0){ 
-    $sql_code = "DELETE FROM cliente WHERE idcliente = '$idCliente' ";
-    $sql_query = $conexao->query($sql_code);
+    $idCliente = isset($_GET['id']) ? $_GET['id'] : 0;
 
-    header("Location: ../../../clientes.php");
+    if($idCliente > 0){ 
+        $sql_code = "DELETE FROM cliente WHERE idcliente = '$idCliente' ";
+        $sql_query = $conexao->query($sql_code);
 
+        header("Location: ../../../clientes.php");
 
+    } else {
+        header("location: ../../../index.php");
+    }
 } else {
-    header("location: ../../../index.php");
+    header("Location: ../../../nao_permitido.php");
 }
+
